2b3a99545b77eaca617e192de4afc906b606ef60
Abhishek <abhishekkrpandey42@gmail.com>
2019-10-06 23:50:20 +0530

    Revert "Knowledge base with crud implementation ."
    This reverts commit ee63b04f

--- src/main/java/com/gen/kb/knowledgebase/FAQRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gen.kb.knowledgebase;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface FAQRepository extends JpaRepository<DocumentTypeFAQ,Integer> {
-}
