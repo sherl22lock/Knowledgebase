f356c05afa53812c2e3c5240828fb666793041c0
Abhishek <abhishekkrpandey42@gmail.com>
2019-10-14 23:12:49 +0530

    changes for junit .

--- src/test/java/com/gen/kb/knowledgebase/KnowledgebaseApplicationTests.java
+++ src/test/java/com/gen/kb/knowledgebase/KnowledgebaseApplicationTests.java
@@ -2,15 +2,66 @@ package com.gen.kb.knowledgebase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.print.Doc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = KnowledgebaseApplication.class)
 public class KnowledgebaseApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+@Autowired
+FAQService faqService;
+
+/*inserting 3 documents and updating the second and deleting the third */
+
+
+@Test
+public void saveIntoDBtest(){
+    DocumentTypeFAQ documentTypeFAQ = new DocumentTypeFAQ();
+    documentTypeFAQ.setQuestion("what is this");
+    documentTypeFAQ.setAnswers("this is a test case");
+    assertEquals(documentTypeFAQ,faqService.saveIntoDB(documentTypeFAQ));
+
+    DocumentTypeFAQ documentTypeFAQ1 = new DocumentTypeFAQ();
+    documentTypeFAQ1.setQuestion("what is this");
+    documentTypeFAQ1.setAnswers("this is a test case 2");
+    assertEquals(documentTypeFAQ1,faqService.saveIntoDB(documentTypeFAQ1));
+
+    DocumentTypeFAQ documentTypeFAQ2 = new DocumentTypeFAQ();
+    documentTypeFAQ2.setQuestion("what is this");
+    documentTypeFAQ2.setAnswers("this is a test case 2");
+    assertEquals(documentTypeFAQ2,faqService.saveIntoDB(documentTypeFAQ2));
+}
+
+/* If id is not specified then, it will throw record not found exception */
+
+@Test
+public void updateIntoDBtest() throws RecordNotFoundException {
+    DocumentTypeFAQ documentTypeFAQ = new DocumentTypeFAQ();
+    documentTypeFAQ.setDocID(2);
+    documentTypeFAQ.setQuestion("what is this test case ?");
+    documentTypeFAQ.setAnswers("this is a update test case");
+    assertNotEquals(documentTypeFAQ,faqService.updateFAQ(documentTypeFAQ));
+}
+
+/* deleting the third document */
+@Test
+public void deleteFAQTest() throws RecordNotFoundException {
+    faqService.deleteById(3);
+}
+/*inserted 3 documents, deleted one document, now retreiving the list and getting the size */
+@Test
+    public void getAllFAQTest(){
+    assertEquals(2,faqService.getAllDFAQ().size());
+}
+
 
 }
