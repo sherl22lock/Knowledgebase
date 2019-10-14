ff85c4ec63a992168fe00f5ffe7fd71533c144ae
Abhishek <abhishekkrpandey42@gmail.com>
2019-10-14 23:25:25 +0530

    changes for integration tests .

---
+++ src/test/java/com/gen/kb/knowledgebase/KnowledgebaseApplicationIntegrationTests.java
@@ -0,0 +1,17 @@
+package com.gen.kb.knowledgebase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = KnowledgebaseApplication.class)
+public class KnowledgebaseApplicationIntegrationTests {
+
+
+}
